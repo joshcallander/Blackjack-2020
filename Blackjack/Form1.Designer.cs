dd3d0c63cb2f003f59f4c44ce7d9a43feb2e2d45
Josh Callander <joshcallander@mmc.school.nz>
2020-09-07 13:50:14 +1200

    Upload

--- Blackjack/Form1.Designer.cs
+++ Blackjack/Form1.Designer.cs
@@ -38,98 +38,135 @@
             this.resetButton = new System.Windows.Forms.Button();
             this.dealButton = new System.Windows.Forms.Button();
             this.PnlGame = new System.Windows.Forms.Panel();
+            this.betValue = new System.Windows.Forms.TextBox();
+            this.lblBetAmount = new System.Windows.Forms.Label();
             this.TmrChips = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.PnlGame.SuspendLayout();
             this.SuspendLayout();
             // 
             // resultLabel
             // 
             this.resultLabel.AutoSize = true;
-            this.resultLabel.Location = new System.Drawing.Point(467, 189);
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(14, 371);
             this.resultLabel.Name = "resultLabel";
-            this.resultLabel.Size = new System.Drawing.Size(35, 13);
+            this.resultLabel.Size = new System.Drawing.Size(117, 18);
             this.resultLabel.TabIndex = 0;
-            this.resultLabel.Text = "label1";
+            this.resultLabel.Text = "Game Loading...";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(126, 52);
+            this.pictureBox1.Location = new System.Drawing.Point(18, 21);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(71, 96);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(319, 69);
+            this.pictureBox2.Location = new System.Drawing.Point(113, 21);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.Size = new System.Drawing.Size(71, 96);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(529, 51);
+            this.pictureBox4.Location = new System.Drawing.Point(206, 21);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.Size = new System.Drawing.Size(71, 96);
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(416, 303);
+            this.startButton.Location = new System.Drawing.Point(345, 263);
             this.startButton.Name = "startButton";
             this.startButton.Size = new System.Drawing.Size(75, 23);
             this.startButton.TabIndex = 4;
-            this.startButton.Text = "button1";
+            this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // PlayerStopButton
             // 
-            this.PlayerStopButton.Location = new System.Drawing.Point(280, 254);
+            this.PlayerStopButton.Location = new System.Drawing.Point(516, 429);
             this.PlayerStopButton.Name = "PlayerStopButton";
             this.PlayerStopButton.Size = new System.Drawing.Size(75, 23);
             this.PlayerStopButton.TabIndex = 5;
-            this.PlayerStopButton.Text = "button1";
+            this.PlayerStopButton.Text = "Stay";
             this.PlayerStopButton.UseVisualStyleBackColor = true;
+            this.PlayerStopButton.Visible = false;
+            this.PlayerStopButton.Click += new System.EventHandler(this.PlayerStopButton_Click);
             // 
             // resetButton
             // 
-            this.resetButton.Location = new System.Drawing.Point(590, 227);
+            this.resetButton.Location = new System.Drawing.Point(713, 429);
             this.resetButton.Name = "resetButton";
             this.resetButton.Size = new System.Drawing.Size(75, 23);
             this.resetButton.TabIndex = 6;
-            this.resetButton.Text = "button1";
+            this.resetButton.Text = "Reset";
             this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Visible = false;
             // 
             // dealButton
             // 
-            this.dealButton.Location = new System.Drawing.Point(633, 321);
+            this.dealButton.Location = new System.Drawing.Point(613, 429);
             this.dealButton.Name = "dealButton";
             this.dealButton.Size = new System.Drawing.Size(75, 23);
             this.dealButton.TabIndex = 7;
-            this.dealButton.Text = "button1";
+            this.dealButton.Text = "Deal";
             this.dealButton.UseVisualStyleBackColor = true;
+            this.dealButton.Visible = false;
             // 
             // PnlGame
             // 
             this.PnlGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(170)))), ((int)(((byte)(85)))));
+            this.PnlGame.Controls.Add(this.betValue);
+            this.PnlGame.Controls.Add(this.lblBetAmount);
+            this.PnlGame.Controls.Add(this.pictureBox1);
+            this.PnlGame.Controls.Add(this.pictureBox2);
+            this.PnlGame.Controls.Add(this.startButton);
+            this.PnlGame.Controls.Add(this.pictureBox4);
+            this.PnlGame.Controls.Add(this.resultLabel);
             this.PnlGame.Location = new System.Drawing.Point(1, 1);
             this.PnlGame.Name = "PnlGame";
-            this.PnlGame.Size = new System.Drawing.Size(799, 400);
+            this.PnlGame.Size = new System.Drawing.Size(799, 410);
             this.PnlGame.TabIndex = 8;
+            this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
+            // 
+            // betValue
+            // 
+            this.betValue.Location = new System.Drawing.Point(345, 224);
+            this.betValue.Name = "betValue";
+            this.betValue.Size = new System.Drawing.Size(100, 20);
+            this.betValue.TabIndex = 5;
+            // 
+            // lblBetAmount
+            // 
+            this.lblBetAmount.AutoSize = true;
+            this.lblBetAmount.Location = new System.Drawing.Point(342, 199);
+            this.lblBetAmount.Name = "lblBetAmount";
+            this.lblBetAmount.Size = new System.Drawing.Size(62, 13);
+            this.lblBetAmount.TabIndex = 4;
+            this.lblBetAmount.Text = "Bet Amount";
             // 
             // TmrChips
             // 
             this.TmrChips.Enabled = true;
+            this.TmrChips.Tick += new System.EventHandler(this.TmrChips_Tick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 427);
+            this.label1.Location = new System.Drawing.Point(26, 439);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 9;
@@ -139,16 +176,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 458);
+            this.ClientSize = new System.Drawing.Size(800, 464);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dealButton);
             this.Controls.Add(this.resetButton);
             this.Controls.Add(this.PlayerStopButton);
-            this.Controls.Add(this.startButton);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.resultLabel);
             this.Controls.Add(this.PnlGame);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -156,6 +188,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.PnlGame.ResumeLayout(false);
+            this.PnlGame.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,6 +208,8 @@
         private System.Windows.Forms.Panel PnlGame;
         private System.Windows.Forms.Timer TmrChips;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox betValue;
+        private System.Windows.Forms.Label lblBetAmount;
     }
 }
 
--- Blackjack/Form1.cs
+++ Blackjack/Form1.cs
@@ -40,86 +40,70 @@ namespace Blackjack
 
         // Declare varibles for welcome screen chips
         Graphics g;
-        Rectangle[] chipContainer = new Rectangle[14];
-        int[] chipSpeed = new int[13]; 
-        int[] chipColour = new int[13];
+        Rectangle[] chipContainer = new Rectangle[10];
+        int[] chipSpeed = new int[9]; 
+        int[] chipColour = new int[9];
         int y = 20, x = 20;
         bool showChips = true;
 
         // Create the list for the deck
         List<Card> deck = new List<Card>()
-            {
-                #region spades
+        {
+            new Card() { Value  = 2, Name = "Two Spades", Image = "2S.png" },
+            new Card() { Value = 3, Name = "Three Spades", Image = "3S.png"},
+            new Card() { Value = 4, Name =  "Four Spades", Image = "4S.png"},
+            new Card() { Value = 5, Name = "Five Spades", Image = "5S.png" },
+            new Card() { Value = 6, Name = "Six Spades", Image = "6S.png" },
+            new Card() { Value = 7, Name = "Seven Spades", Image = "7S.png" },
+            new Card() { Value = 8, Name = "Eight Spades", Image = "8S.png" },
+            new Card() { Value = 9, Name = "Nine Spades", Image = "9S.png" },
+            new Card() { Value = 10, Name = "Ten Spades", Image = "10S.png" },
+            new Card() { Value = 10, Name = "Jack Spades", Image = "JS.png" },
+            new Card() { Value = 10, Name = "Queen Spades", Image = "QS.png" },
+            new Card(){ Value = 10, Name = "King Spades", Image = "KS.png" },
+            new Card(){ Value = 11, Name = "Ace Spades", Image = "AS.png" },
 
-                new Card() { Value  = 2, Name = "Two Spades", Image = "2S.png" },
-                new Card() { Value = 3, Name = "Three Spades", Image = "3S.png"},
-                new Card() { Value = 4, Name =  "Four Spades", Image = "4S.png"},
-                new Card() { Value = 5, Name = "Five Spades", Image = "5S.png" },
-                new Card() { Value = 6, Name = "Six Spades", Image = "6S.png" },
-                new Card() { Value = 7, Name = "Seven Spades", Image = "7S.png" },
-                new Card() { Value = 8, Name = "Eight Spades", Image = "8S.png" },
-                new Card() { Value = 9, Name = "Nine Spades", Image = "9S.png" },
-                new Card() { Value = 10, Name = "Ten Spades", Image = "10S.png" },
-                new Card() { Value = 10, Name = "Jack Spades", Image = "JS.png" },
-                new Card() { Value = 10, Name = "Queen Spades", Image = "QS.png" },
-                new Card(){ Value = 10, Name = "King Spades", Image = "KS.png" },
-                new Card(){ Value = 11, Name = "Ace Spades", Image = "AS.png" },
+            new Card() { Value  = 2, Name = "Two Diamonds", Image = "2D.png" },
+            new Card() { Value = 3, Name = "Three Diamonds", Image = "3D.png" },
+            new Card() { Value = 4, Name =  "Four Diamonds", Image = "4D.png"},
+            new Card() { Value = 5, Name = "Five Diamonds", Image = "5D.png" },
+            new Card() { Value = 6, Name = "Six Diamonds", Image = "6D.png" },
+            new Card(){ Value = 7, Name = "Seven Diamonds", Image = "7D.png" },
+            new Card() { Value = 8, Name = "Eight Diamonds", Image = "8D.png" },
+            new Card() { Value = 9, Name = "Nine Diamonds", Image = "9D.png" },
+            new Card() { Value = 10, Name = "Ten Diamonds", Image = "10D.png" },
+            new Card() { Value = 10, Name = "Jack Diamonds", Image = "JD.png" },
+            new Card() { Value = 10, Name = "Queen Diamonds", Image = "QD.png" },
+            new Card(){ Value = 10, Name = "King Diamonds", Image = "KD.png" },
+            new Card(){ Value = 11, Name = "Ace Diamonds", Image = "AD.png" },
 
-                #endregion
+            new Card() { Value  = 2, Name = "Two Clubs", Image = "2C.png" },
+            new Card() { Value = 3, Name = "Three Clubs", Image = "3C.png" },
+            new Card() { Value = 4, Name =  "Four Clubs", Image = "4C.png"},
+            new Card() { Value = 5, Name = "Five Clubs", Image = "5C.png" },
+            new Card() { Value = 6, Name = "Six Clubs", Image = "6C.png" },
+            new Card(){ Value = 7, Name = "Seven Clubs", Image = "7C.png" },
+            new Card() { Value = 8, Name = "Eight Clubs", Image = "8C.png" },
+            new Card() { Value = 9, Name = "Nine Clubs", Image= "9C.png" },
+            new Card() { Value = 10, Name = "Ten Clubs", Image = "10C.png" },
+            new Card() { Value = 10, Name = "Jack Clubs", Image = "JC.png" },
+            new Card() { Value = 10, Name = "Queen Clubs", Image = "QC.png" },
+            new Card(){ Value = 10, Name = "King Clubs", Image = "KC.png" },
+            new Card(){ Value = 11, Name = "Ace Clubs", Image = "AC.png" },
 
-                #region diamonds
-
-                new Card() { Value  = 2, Name = "Two Diamonds", Image = "2D.png" },
-                new Card() { Value = 3, Name = "Three Diamonds", Image = "3D.png" },
-                new Card() { Value = 4, Name =  "Four Diamonds", Image = "4D.png"},
-                new Card() { Value = 5, Name = "Five Diamonds", Image = "5D.png" },
-                new Card() { Value = 6, Name = "Six Diamonds", Image = "6D.png" },
-                new Card(){ Value = 7, Name = "Seven Diamonds", Image = "7D.png" },
-                new Card() { Value = 8, Name = "Eight Diamonds", Image = "8D.png" },
-                new Card() { Value = 9, Name = "Nine Diamonds", Image = "9D.png" },
-                new Card() { Value = 10, Name = "Ten Diamonds", Image = "10D.png" },
-                new Card() { Value = 10, Name = "Jack Diamonds", Image = "JD.png" },
-                new Card() { Value = 10, Name = "Queen Diamonds", Image = "QD.png" },
-                new Card(){ Value = 10, Name = "King Diamonds", Image = "KD.png" },
-                new Card(){ Value = 11, Name = "Ace Diamonds", Image = "AD.png" },
-
-                #endregion
-
-                #region clubs
-
-                new Card() { Value  = 2, Name = "Two Clubs", Image = "2C.png" },
-                new Card() { Value = 3, Name = "Three Clubs", Image = "3C.png" },
-                new Card() { Value = 4, Name =  "Four Clubs", Image = "4C.png"},
-                new Card() { Value = 5, Name = "Five Clubs", Image = "5C.png" },
-                new Card() { Value = 6, Name = "Six Clubs", Image = "6C.png" },
-                new Card(){ Value = 7, Name = "Seven Clubs", Image = "7C.png" },
-                new Card() { Value = 8, Name = "Eight Clubs", Image = "8C.png" },
-                new Card() { Value = 9, Name = "Nine Clubs", Image= "9C.png" },
-                new Card() { Value = 10, Name = "Ten Clubs", Image = "10C.png" },
-                new Card() { Value = 10, Name = "Jack Clubs", Image = "JC.png" },
-                new Card() { Value = 10, Name = "Queen Clubs", Image = "QC.png" },
-                new Card(){ Value = 10, Name = "King Clubs", Image = "KC.png" },
-                new Card(){ Value = 11, Name = "Ace Clubs", Image = "AC.png" },
-
-                #endregion
-
-                #region hearts
-
-                new Card() { Value  = 2, Name = "Two Hearts", Image = "2H.png" },
-                new Card() { Value = 3, Name = "Three Hearts", Image = "3H.png" },
-                new Card() { Value = 4, Name =  "Four Hearts", Image = "4H.png"},
-                new Card() { Value = 5, Name = "Five Hearts", Image = "5H.png" },
-                new Card() { Value = 6, Name = "Six Hearts", Image = "6H.png" },
-                new Card(){ Value = 7, Name = "Seven Hearts", Image = "7H.png" },
-                new Card() { Value = 8, Name = "Eight Hearts", Image = "8H.png" },
-                new Card() { Value = 9, Name = "Nine Hearts", Image = "9H.png" },
-                new Card() { Value = 10, Name = "Ten Hearts", Image = "10H.png" },
-                new Card() { Value = 10, Name = "Jack Hearts", Image = "JH.png" },
-                new Card() { Value = 10, Name = "Queen Hearts", Image = "QH.png" },
-                new Card(){ Value = 10, Name = "King Hearts", Image = "KH.png" },
-                new Card(){ Value = 11, Name = "Ace Hearts", Image = "AH.png" }
-
-                #endregion
+            new Card() { Value  = 2, Name = "Two Hearts", Image = "2H.png" },
+            new Card() { Value = 3, Name = "Three Hearts", Image = "3H.png" },
+            new Card() { Value = 4, Name =  "Four Hearts", Image = "4H.png"},
+            new Card() { Value = 5, Name = "Five Hearts", Image = "5H.png" },
+            new Card() { Value = 6, Name = "Six Hearts", Image = "6H.png" },
+            new Card(){ Value = 7, Name = "Seven Hearts", Image = "7H.png" },
+            new Card() { Value = 8, Name = "Eight Hearts", Image = "8H.png" },
+            new Card() { Value = 9, Name = "Nine Hearts", Image = "9H.png" },
+            new Card() { Value = 10, Name = "Ten Hearts", Image = "10H.png" },
+            new Card() { Value = 10, Name = "Jack Hearts", Image = "JH.png" },
+            new Card() { Value = 10, Name = "Queen Hearts", Image = "QH.png" },
+            new Card(){ Value = 10, Name = "King Hearts", Image = "KH.png" },
+            new Card(){ Value = 11, Name = "Ace Hearts", Image = "AH.png" }
         };
 
         public Form1()
@@ -129,10 +113,10 @@ namespace Blackjack
             // Remove double buffering
             typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, PnlGame, new object[] { true });
 
-            for (int i = 0; i <= 12; i++)
+            for (int i = 0; i <= 8; i++)
             {
                 // Declare the rectangle to put the shark image in for each shark
-                chipContainer[i] = new Rectangle(y, x + 46 * i, 30, 30);
+                chipContainer[i] = new Rectangle(y, x + 49 * i, 30, 30);
             }
         }
 
@@ -149,7 +133,7 @@ namespace Blackjack
             if (showChips == true)
             {
                 // Use the DrawImage method to draw each of the sharks onto the panel
-                for (int i = 0; i <= 12; i++)
+                for (int i = 0; i <= 8; i++)
                 {
                     chipColour[i] = random.Next(1, 5);
 
@@ -185,8 +169,8 @@ namespace Blackjack
         {
             if (showChips == true)
             {
-                for (int i = 0; i <= 12; i++)
-                { // Run the code for each of the 5 sharks
+                for (int i = 0; i <= 8; i++)
+                {
                     chipSpeed[i] = random.Next(10, 23);
 
                     chipContainer[i].X += chipSpeed[i]; // Set each shark to the correct speed
@@ -209,12 +193,67 @@ namespace Blackjack
                 resultLabel.Text = String.Format("Already started.");
             }
 
+            else if (betValue.Text == "" || betValue.Text == "0")
+            {
+                resultLabel.Text = String.Format("Please enter a valid bet amount");
+            }
+
             else
             {
                 playercardSum = 0;
                 bankercardSum = 0;
 
+                int randomCard1 = selectRandomCard();
+                Card card1 = deck[randomCard1];
+                usedCards.Add(randomCard1);
+                int randomCard2 = selectRandomCard();
+
+
+                while (usedCards.Contains(randomCard2))
+                {
+                    randomCard2 = selectRandomCard();
+                }
+
+                randomCard2 = 1 * randomCard2;
+
+                Card card2 = deck[randomCard2];
+                usedCards.Add(randomCard2);
+
+                playercardList.Add(card1);
+                playercardList.Add(card2);
+
+                pictureBox1.ImageLocation = card1.Image;
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+                pictureBox2.ImageLocation = card2.Image;
+                pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+
+                int randomCard3 = selectRandomCard();
+                while (usedCards.Contains(randomCard3))
+                {
+                    randomCard3 = selectRandomCard();
+                }
+
+                randomCard3 = 1 * randomCard3;
+                Card card3 = deck[randomCard3];
+                usedCards.Add(randomCard3);
+
+                bankercardList.Add(card3);
+
+                pictureBox4.ImageLocation = card3.Image;
+                pictureBox4.SizeMode = PictureBoxSizeMode.AutoSize;
+
                 sumPlayerCards();
+
+                PlayerStopButton.Visible = true;
+                dealButton.Visible = true;
+                resetButton.Visible = true;
+
+                lblBetAmount.Visible = false;
+                betValue.Visible = false;
+                startButton.Visible = false;
+
+                showChips = false;
 
                 if (playercardSum == 21)
                 {
@@ -318,7 +357,9 @@ namespace Blackjack
                     playerbox.Add(p3);
 
                     playercardList.Add(card);
+
                     sumPlayerCards();
+
                     if (playercardSum > 21)
                     {
                         resultLabel.Text = String.Format("The sum of your cards is: {0}, you lose!", playercardSum);
@@ -367,7 +408,7 @@ namespace Blackjack
             playercardList.Clear();
             bankercardList.Clear();
             usedCards.Clear();
-            resultLabel.Text = "Player choice";
+            resultLabel.Text = "Please enter bet amount";
 
             chipCount = chipCount + bet;
         }
